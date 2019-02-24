316b23dfc92f2f9ad9636d602bed518d308e5c14
Silvio Henrique Ferreira <shferreira@me.com>
2019-02-24 14:31:56 -0300

    Use the compiler to clean up unused rules

--- src/purifycss.js
+++ src/purifycss.js
@@ -1,68 +1,41 @@
-const fs = require("fs");
 import parse from "./utils/parse";
 import compile from "./utils/compile";
 import FileUtil from "./utils/FileUtil";
 
 const RULE_TYPE = "rule";
 const MEDIA_TYPE = "media";
+const NON_ALPHA = /[^a-z]/g;
 
-const getAllWordsInContent = content =>
-  content.split(/[^a-z]/g).concat(["body", "html"]);
-
-var getAllWordsInSelector = selector => {
-  return selector
+const getAllWordsInSelector = selector =>
+  selector
     .replace(/\[(.*?)\]/g, "")
     .replace(/[:*][a-z-_:]+/g, "")
     .toLowerCase()
     .match(/[a-z]+/g) || [];
-}
 
-const readRules = (rules, filter) => {
+const readRules = (rules, wordsInContent) => {
   for (let rule of rules) {
     if (rule.type === RULE_TYPE) {
-      filter(rule);
+      rule.selectors = rule.selectors.filter(selector =>
+        getAllWordsInSelector(selector).every(word =>
+          wordsInContent.includes(word)
+        )
+      );
     } else if (rule.type === MEDIA_TYPE) {
-      readRules(rule.rules, filter);
+      readRules(rule.rules, wordsInContent);
     }
   }
-};
-
-const removeEmptyRules = rules => {
-  let emptyRules = [];
-  for (let rule of rules) {
-    if (rule.type === RULE_TYPE && rule.selectors.length === 0) {
-      emptyRules.push(rule);
-    } else if (rule.type === MEDIA_TYPE) {
-      removeEmptyRules(rule.rules);
-      if (rule.rules.length === 0) {
-        emptyRules.push(rule);
-      }
-    }
-  }
-  emptyRules.forEach(emptyRule => rules.splice(rules.indexOf(emptyRule), 1));
-};
-
-const filterSelectors = (selectors, contentWords) => {
-  return selectors.filter(selector => {
-    const words = getAllWordsInSelector(selector);
-    const usedWords = words.filter(word => contentWords.includes(word));
-    return usedWords.length === words.length;
-  });
 };
 
 export default (searchThrough, css, options = {}) => {
-  const whitelist = options.whitelist || [];
+  const whitelist = ["html", "body"].concat(options.whitelist || []);
   const cssString = FileUtil.filesToSource(css);
   const content = FileUtil.filesToSource(searchThrough).toLowerCase();
-  const wordsInContent = getAllWordsInContent(content).concat(whitelist);
+  const wordsInContent = content.split(NON_ALPHA).concat(whitelist);
 
   const ast = parse(cssString);
   const rules = ast.stylesheet.rules;
-  readRules(
-    rules,
-    rule => (rule.selectors = filterSelectors(rule.selectors, wordsInContent))
-  );
-  removeEmptyRules(rules);
+  readRules(rules, wordsInContent);
 
   return compile(ast);
 };
--- src/utils/compile.js
+++ src/utils/compile.js
@@ -16,94 +16,72 @@ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 */
 
-export default (node, options) => new Compiler(options).compile(node);
-
-function Compiler(options = {}) {
-  this.options = options;
-}
-
-Compiler.prototype.compile = function(node) {
-  return this.stylesheet(node);
+const compiler = {
+  compile(node) {
+    return this.stylesheet(node);
+  },
+  stylesheet(node) {
+    return this.mapVisit(node.stylesheet.rules);
+  },
+  mapVisit(nodes) {
+    return nodes.map(node => this[node.type](node)).join("");
+  },
+  comment() {
+    return "";
+  },
+  import(node) {
+    return "@import " + node.import + ";";
+  },
+  media({ media, rules }) {
+    const result = this.mapVisit(rules);
+    if (!result.length) return "";
+    return "@media " + media + "{" + result + "}";
+  },
+  document({ vendor, document, rules }) {
+    if (!rules.length) return "";
+    var doc = "@" + (vendor || "") + "document " + document;
+    return doc + (" " + " {") + this.mapVisit(rules) + "}";
+  },
+  charset({ charset }) {
+    return "@charset " + charset + ";";
+  },
+  namespace({ namespace }) {
+    return "@namespace " + namespace + ";";
+  },
+  supports({ supports, rules }) {
+    if (!rules.length) return "";
+    return "@supports " + supports + " {" + this.mapVisit(rules) + "}";
+  },
+  keyframes({ vendor = "", name, keyframes }) {
+    return (
+      "@" + vendor + "keyframes " + name + " {" + this.mapVisit(keyframes) + "}"
+    );
+  },
+  keyframe({ declarations, values }) {
+    return values.join(", ") + " {" + this.mapVisit(declarations) + "}";
+  },
+  page({ selectors, declarations }) {
+    if (selectors.length == 0) return "";
+    var sel = selectors.length ? selectors.join(", ") + " " : "";
+    return "@page " + sel + "{" + this.mapVisit(declarations) + "}";
+  },
+  "font-face"({ declarations }) {
+    return "@font-face " + "{" + this.mapVisit(declarations) + "}";
+  },
+  host({ rules }) {
+    if (!rules.length) return "";
+    return "@host" + " {" + this.mapVisit(rules) + "}";
+  },
+  "custom-media"({ name, media }) {
+    return "@custom-media " + name + " " + media + ";";
+  },
+  rule({ declarations, selectors }) {
+    if (!declarations.length || !selectors.length) return "";
+    return selectors.join(",") + "{" + this.mapVisit(declarations) + "}";
+  },
+  declaration({ property, value }) {
+    return property + ": " + value + ";";
+  }
 };
 
-Compiler.prototype.stylesheet = function(node) {
-  return this.mapVisit(node.stylesheet.rules);
-};
-
-Compiler.prototype.mapVisit = function(nodes) {
-  return nodes.map(node => this[node.type](node)).join("");
-};
-
-Compiler.prototype.comment = function() {
-  return "";
-};
-
-Compiler.prototype.import = function(node) {
-  return "@import " + node.import + ";";
-};
-
-Compiler.prototype.media = function({ media, rules }) {
-  return "@media " + media + "{" + this.mapVisit(rules) + "}";
-};
-
-Compiler.prototype.document = function({ vendor, document, rules }) {
-  var doc = "@" + (vendor || "") + "document " + document;
-
-  return doc + (" " + " {") + this.mapVisit(rules) + "}";
-};
-
-Compiler.prototype.charset = function({ charset }) {
-  return "@charset " + charset + ";";
-};
-
-Compiler.prototype.namespace = function({ namespace }) {
-  return "@namespace " + namespace + ";";
-};
-
-Compiler.prototype.supports = function({ supports, rules }) {
-  return "@supports " + supports + " {" + this.mapVisit(rules) + "}";
-};
-
-Compiler.prototype.keyframes = function({ vendor, name, keyframes }) {
-  return (
-    "@" +
-    (vendor || "") +
-    "keyframes " +
-    name +
-    " {" +
-    this.mapVisit(keyframes) +
-    "}"
-  );
-};
-
-Compiler.prototype.keyframe = function({ declarations, values }) {
-  return values.join(", ") + " {" + this.mapVisit(declarations) + "}";
-};
-
-Compiler.prototype.page = function({ selectors, declarations }) {
-  var sel = selectors.length ? selectors.join(", ") + " " : "";
-
-  return "@page " + sel + "{" + this.mapVisit(declarations) + "}";
-};
-
-Compiler.prototype["font-face"] = function({ declarations }) {
-  return "@font-face " + "{" + this.mapVisit(declarations) + "}";
-};
-
-Compiler.prototype.host = function({ rules }) {
-  return "@host" + " {" + this.mapVisit(rules) + "}";
-};
-
-Compiler.prototype["custom-media"] = function({ name, media }) {
-  return "@custom-media " + name + " " + media + ";";
-};
-
-Compiler.prototype.rule = function({ declarations, selectors }) {
-  if (!declarations.length) return "";
-
-  return selectors.join(",") + "{" + this.mapVisit(declarations) + "}";
-};
-
-Compiler.prototype.declaration = function({ property, value }) {
-  return property + ": " + value + ";";
-};
+export default node => compiler.compile(node);
